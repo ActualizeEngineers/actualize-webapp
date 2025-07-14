904322584debc2a1a58717f1ce928fef091a6e8d
Trainee <Trainee@actualize.local>
2025-07-14 13:52:03 +0530

    First design of cards of why choose actualize

---
+++ src/components/shared/Card.tsx
@@ -0,0 +1,20 @@
+  import '../../styles/Card.css'
+ type CardProps = {
+  img: string;
+  title: string;
+  desc: string;
+};
+ export default function Card({ img, title, desc }: CardProps)
+ {
+return (
+ 
+ <div className="card ">
+    <img src={img} alt="Sample Image" />
+    <div className="card-content">
+      <h2 className="card-title">{title}</h2>
+      <p className="card-description">
+        {desc}
+      </p>
+    </div>
+  </div>
+ )}
