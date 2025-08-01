b5b4726b4f636aa92fde4af8fb1678f38cea1c58
VigneshActualize <vignesh.n@actualize.co.in>
2025-08-01 17:08:22 +0530

    Updated Global Customers into CustomerCarousel

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles/custom.css';
 import Footer from './layouts/Footer';
-import FloatingActions from './components/shared/FloatingActions';
+// import FloatingActions from './components/shared/FloatingActions';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
       <Footer />
-      <FloatingActions />
+      {/* <FloatingActions /> */}
     </BrowserRouter>
   </React.StrictMode>
 );
