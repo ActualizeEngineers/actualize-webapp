e9050afdfde99f81cd219938da93341d863deb31
Trainee <Trainee@actualize.local>
2025-08-01 17:13:31 +0530

    Changes in Footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -10,7 +10,7 @@ const Footer: React.FC = () => {
         {/* Brand + Tagline Section */}
         <div className="footer-brand">
           <div className="footer-brand-content">
-            <img src={logo} alt="Actualize Logo" className="footer-logo" style={{ margin: '5px' }}/>
+            <img src={logo} alt="Actualize Logo" className="footer-logo" style={{ margin: '5px' }} />
             <div className="footer-brand-text">
               <h2>Actualize</h2>
               <p>
@@ -24,24 +24,36 @@ const Footer: React.FC = () => {
         {/* Company Links */}
         <div className="footer-links">
           <h4>Company</h4>
-          <NavLink to="/company/about">About</NavLink>
-          <NavLink to="/company/team">Team</NavLink>
-          <NavLink to="/company/life-at-actualize">Life at Actualize</NavLink>
+          <NavLink to="/company/about" target="_top">About</NavLink>
+          <NavLink to="/company/team" target="_top">Team</NavLink>
+          <NavLink to="/company/life-at-actualize" target="_top">Life at Actualize</NavLink>
+        </div>
+        {/* Services Links */}
+        <div className="footer-links">
+          <h4>Services</h4>
+          <NavLink to="/services/engineering" target="_top">Engineering Services</NavLink>
+          <NavLink to="/services/digital-transformation" target="_top">Digital Transformation</NavLink>
+          <NavLink to="/services/software-development" target="_top"> Software Development</NavLink>
+          <NavLink to="/services/product-development" target="_top">  Product Development</NavLink>
+
         </div>
 
         {/* Services Links */}
         <div className="footer-links">
-          <h4>Services</h4>
-          <NavLink to="/services/engineering">Engineering</NavLink>
-          <NavLink to="/services/digital-transformation">Digital</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
+          <h4>Industries</h4>
+          <NavLink to="/industries/heavy-engineering" target="_top"> Heavy Engineering</NavLink>
+          <NavLink to="/industries/automotive-engineering" target="_top"> Automotive Engineering</NavLink>
+          <NavLink to="/industries/industrial-engineering" target="_top"> Industrial Engineering</NavLink>
+          <NavLink to="/industries/process-engineering" target="_top"> Process Engineering</NavLink>
+
         </div>
 
         {/* Contact Info */}
-        <div className="footer-contact">
+        <div className="footer-links">
           <h4>Contact</h4>
-          <p>📧 contact@actualize.com</p>
-          <p>📞 +91-9XX-XXX-XXXX</p>
+          <NavLink to="" style={{ margin: 0, fontWeight: 0 }}>📧 contact@actualize.com</NavLink>
+          <NavLink to="" style={{ margin: 0 }}>📞 +91-9XX-XXX-XXXX</NavLink>
+          <NavLink to="/contact" target="_top"> ➡️ Contact-US</NavLink>
         </div>
       </div>
 
